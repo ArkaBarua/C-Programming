d3ab00704e79c140aecf9c870b051b7f47d179e7
Arka Barua <arka@pop-os.localdomain>
2021-04-03 19:59:28 +0600

    updated programm

--- projects_c_1/even-odd.c
+++ projects_c_1/even-odd.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
 int main(){
+	char ch = 'c';
     int number;
 
+while(ch == 'c'){
     printf("Enter an Integer number to know it's Even or Odd\n");
     scanf("%d", &number);
 
@@ -12,8 +14,11 @@ int main(){
         printf("The number is a Even number\n");
         break;
     
-    default:
+    case 1:
         printf("The number is a Odd number\n");
     }
+    printf("Enter c to continue.Or q for quite:");
+    scanf(" %c",&ch);
+}
     return 0;
 }
