b0398ee66fb7e46e0574d5242cab69515bbb97e6
Arka Barua <arka@pop-os.localdomain>
2021-04-05 17:13:23 +0600

    created a diamond programm

---
+++ project_c_2/diamond.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+
+    int i , j, rows;
+    printf("Enter row number to create a diamond:");
+    scanf("%d",&rows);
+
+    int stars, spaces;
+
+    stars = 1;
+    spaces = rows -1;
+
+    for(i = 1; i < rows * 2 ; i++){
+        for (j = 1; j <= spaces; j++){
+            printf(" ");
+}
+        for(j = 1;j < stars * 2;j++){
+            printf("*");
+        }
+        if(i < rows){
+            spaces--;
+            stars++;
+        }else{
+            spaces++;
+            stars--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
