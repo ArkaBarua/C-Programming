b0fdb477c04b76f96938660ccf3fcee196a4dc4e
Arka Barua <arka@pop-os.localdomain>
2021-04-08 11:57:48 +0600

    Learned Continue and Break in while loop

---
+++ loop/while_loop/conitnur_break.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+int i,j;
+i = 1;
+j = 100;
+    for ( i; i <= j; i++)
+    {
+        if (i % 2)
+        {
+        continue;
+        }
+    
+ 
+    printf("I = %d\n",i);
+    //   if (i==5){
+    //    break;
+    //}
+
+    }
+return 0;
+}
