ae53e925366578cb117cbdfa5c301c0d0b54c0f4
Arka Barua <arka@pop-os.localdomain>
2021-04-28 10:08:16 +0600

    finished the travers_array

--- array/travers_array.c
+++ array/travers_array.c
@@ -1,38 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int scores[5];
-    scores[0]= 81;
-    scores[1]= 83;
-    scores[2]= 85;
-    scores[3]= 89;
-    scores[4]= 91;
+    scores[0] = 81;
+    scores[1] = 83;
+    scores[2] = 85;
+    scores[3] = 89;
+    scores[4] = 91;
 
     //find sum of Scores
-    int sum =0, i;
-    for(i=0;i < 5;i++){
+    int sum = 0, i;
+    for (i = 0; i < 5; i++)
+    {
         sum += scores[i];
     }
-    printf("Sum = %d\n",sum);
+    printf("Sum = %d\n", sum);
 
     //find average score
-    printf("Average Score = %d\n",sum/i);
+    printf("Average Score = %d\n", sum / i);
     //find highest score
-    int max= scores[0];
-    for(i =0;i < 5;i++){
-        if(max < scores[i]){
+    int max = scores[0];
+    for (i = 0; i < 5; i++)
+    {
+        if (max < scores[i])
+        {
             max = scores[i];
         }
     }
-    printf("Highest score = %d",max);
+    printf("Highest score = %d\n", max);
 
     //minimum score
     int min = scores[0];
-    for(i = 0; i < 5;i++){
-        if(min < scores[i]){
-            
+    for (i = 0; i < 5; i++)
+    {
+        if (min < scores[i])
+        {
         }
-
     }
+    printf("Minium Score = %d\n", min);
     return 0;
 }
