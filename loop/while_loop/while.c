0591b419fe1a662f89a1e5bb748edb00004888c6
Arka Barua <arka@pop-os.localdomain>
2021-04-28 09:45:23 +0600

    fixed some problems in while_loop calculator

--- loop/while_loop/while.c
+++ loop/while_loop/while.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
@@ -6,30 +6,36 @@ int main()
     char operator;
     char ch = 'c';
 
-        while (ch != 'q'){
-        printf("Enter your operations +, -, /, * ( 10 + 20) :\n");
-        scanf("%lf %c %lf", &first_num, &operator, &second_num);
+    while (ch == 'c')
+    {
+        if (ch == 'c' || 'q')
+        { //if user enters c or q the program will run or else it will tell an error
 
-        switch (operator)
-        {
+            printf("Enter your operations +, -, /, * ( 10 + 20) :\n");
+            scanf("%lf %c %lf", &first_num, &operator, & second_num);
+
+            switch (operator)
+            {
             case '+':
-        printf("%f\n", first_num + second_num);
-        break;
+                printf("%f\n", first_num + second_num);
+                break;
             case '-':
-        printf("%f\n", first_num + second_num);
-        break;
+                printf("%f\n", first_num - second_num);
+                break;
             case '/':
-        printf("%f\n", first_num + second_num);
-        break;
+                printf("%f\n", first_num / second_num);
+                break;
             case '*':
-        printf("%f\n", first_num + second_num);
-        break;
-            default :
-        printf("There is an error , please Check your operation\n");
-        break;
+                printf("%f\n", first_num * second_num);
+                break;
+            default:
+                printf("There is an error , please Check your operation\n");
+                break;
+            }
         }
         printf("Type c to continue. Type q quite: ");
         scanf(" %c", &ch);
-        }
+    }
+
     return 0;
 }
