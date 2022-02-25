c3e7bfe89d8fd21adb354371de0a18ed63f6e359
Arka Barua <arka.barua83@gmail.com>
2022-02-25 19:52:37 +0600

    learned more about funcitons;

--- function/basicfun.c
+++ function/basicfun.c
@@ -1,4 +1,5 @@
- #include <stdio.h>  
+#include <stdio.h>
+// I wrote a function to get two numbers sum
 double add(double num1, double num2)   
  {  
      double sum = num1 + num2; 
@@ -7,8 +8,9 @@ double add(double num1, double num2)
  int main()   
  {  
      double a, b, c;
+    printf("Enter two numbers: ");
     scanf("%lf %lf",&a,&b);  
-     c = add(a, b);  
-     printf("%lf\n", c);  
-     return 0;  
+    c = add(a, b); // c vars value will be the result of add function
+    printf("%lf\n", c);
+    return 0;
  }
---
+++ function/basicfun2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+double add(double num, double num1);// this is a prototype
+int main(){
+    
+    double a,b,c;
+    printf("Please Enter 2 numbers: ");
+    scanf("%lf %lf",&a, &b);
+    c = add(a, b);
+    printf("%lf\n",c);
+    return 0;
+}
+// we make function after the main function if we write a prototype at the first place
+double add(double num1, double num2){
+    double sum = num1 + num2;
+    return sum;
+}
