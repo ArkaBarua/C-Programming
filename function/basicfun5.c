0d009c077079181c036af6ca2a7530e80b6c0731
Arka Barua <arka.barua83@gmail.com>
2022-03-01 19:42:09 +0600

    learned about global variable

---
+++ function/basicfun5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+double pi = 3.14;// pi is now a global variable
+void my_fun(){
+    pi = 3.1416;// this is a local variable
+    return;
+}
+int main(){
+    printf("%lf\n",pi);
+    my_fun();
+    printf("%lf\n",pi);
+    return 0;
+}
